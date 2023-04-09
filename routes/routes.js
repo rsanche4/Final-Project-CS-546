cdcaaae0f5c9285ae31427375aee654ef11a7bb5
PawanPerera <pperera@stevens.edu>
2023-04-08 21:41:46 -0400

    added HTML templates, some css, and changed the '/' route

--- routes/routes.js
+++ routes/routes.js
@@ -4,24 +4,17 @@ const router = Router();
 router
   .route('/')
   .get(async (req, res) => {
-    try {
-      const r = {
-        "TEST": "MAIN ROUTE BARS MAIN PAGE",
-      }
-      res.json(r);
-    } catch (e) {
-      // Something went wrong with the server!
-      res.status(500).send(e);
-    }
+    //need to add bars database as source to put render on the route (see views\homepage.handlebars)
+    res.render('homepage', {title: "Hoboken Bar Review"})
   })
-  .post(async (req, res) => {
-    // Not implemented
-    res.send('POST request to http://localhost:3000/bars/');
-  })
-  .delete(async (req, res) => {
-    // Not implemented
-    res.send('DELETE request to http://localhost:3000/bars/');
-  });
+  // .post(async (req, res) => {
+  //   // Not implemented
+  //   res.send('POST request to http://localhost:3000/bars/');
+  // })
+  // .delete(async (req, res) => {
+  //   // Not implemented
+  //   res.send('DELETE request to http://localhost:3000/bars/');
+  // });
 
   router
   .route('/:id')
